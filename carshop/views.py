c80787fa716559b88a3be578e005a4bdb60d5c49
Mugengano18 <mugenganoa@gmail.com>
2022-11-03 15:03:21 +0200

    made new templates

--- carshop/views.py
+++ carshop/views.py
@@ -2,7 +2,11 @@ from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+def map(request):
+    return re
