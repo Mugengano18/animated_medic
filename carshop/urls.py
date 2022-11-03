2c75d6b0f0bd6127bbc41345b8dd55b16dd45e58
Mugengano18 <mugenganoa@gmail.com>
2022-11-03 15:27:13 +0200

    Added a new form

--- carshop/urls.py
+++ carshop/urls.py
@@ -3,4 +3,5 @@ from carshop import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("shop_register/",views.shop_register, name="shop"),
 ]
--- carshop/views.py
+++ carshop/views.py
@@ -1,12 +1,19 @@
 from http.client import HTTPResponse
+from urllib.request import Request
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'index.html')
 
-def map(request):
-    return re
+def home(request):
+    return render(request, "index.html")
+
+
+def shop_register(request):
+    return render(request, "shop_registration.html")
+
+
+def user_register(request):
+    return render(request, "user_reg.html")
